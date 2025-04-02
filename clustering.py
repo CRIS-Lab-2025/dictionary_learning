f63fee85b0098ab8f78c67488067a41e1383acc4
V Sai Vignesh <sv2795@columbia.edu>
2025-04-02 18:04:49 -0400

    Trying to use sparse reps instead of final layer

--- clustering.py
+++ clustering.py
@@ -1,5 +1,6 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from dictionary import AutoEncoder
 import gc
 
 
@@ -11,6 +12,11 @@ class Pythia_model:
         self.model.to(device)
         self.model.eval()
         self.device = device
+        # Get the sparse encoder trained on the 5th layer.
+        self.sae = AutoEncoder.from_pretrained(
+            "dictionaries/pythia-70m-deduped/mlp_out_layer5/10_32768/ae.pt",
+            map_location=device
+        )
 
     def tokenize(self, text):
         # print("getting inputs ids")
@@ -29,15 +35,7 @@ class Pythia_model:
         tokenized_input = self.tokenize(sentence)
         tokenized_input = tokenized_input.to(self.device)
         # print("tokenized input", tokenized_input)
-        keyword = keyword.lower()
-        # Get the positions corresponding to the keywors
-        decoded_strings = self.tokenizer.convert_ids_to_tokens(tokenized_input[0])
-        # print("deoded strings", decoded_strings)
-        position = -1
-        for i in range(len(decoded_strings)):
-            if keyword in decoded_strings[i]:
-                # print("Found the position of the keyword", keyword, "at position", i)
-                position = i
+        position = self.get_keyword_position(sentence, keyword)
         if position == -1:
             return None
 
@@ -49,7 +47,37 @@ class Pythia_model:
         # clean up code
         finally:
             del tokenized_input
-            del decoded_strings
             gc.collect()
             torch.cuda.empty_cache()
-        return final_layer_activations[0][position].cpu().numpy()
+        return final_layer_activations[0][position]
+
+    def get_keyword_position(self, sentence, keyword):
+        # Identify the position of the keyword in the tokenized input
+        # We need to convert the tensor to the token string
+        tokenized_input = self.tokenize(sentence)
+        tokenized_input = tokenized_input.to(self.device)
+        # print("tokenized input", tokenized_input)
+        keyword = keyword.lower()
+        # Get the positions corresponding to the keywords
+        decoded_strings = self.tokenizer.convert_ids_to_tokens(tokenized_input[0])
+        # print("decoded strings", decoded_strings)
+        position = -1
+        for i in range(len(decoded_strings)):
+            if keyword in decoded_strings[i]:
+                # print("Found the position of the keyword", keyword, "at position", i)
+                position = i
+        return position
+
+    def get_sparse_activations_for_token(self, sentence, keyword):
+        # Identify the position of the keyword in the tokenized input
+        # We need to convert the tensor to the token string
+        activations = self.get_activation_for_token(sentence, keyword)
+        if activations is None:
+            return None
+        # Get the sparse rep of these tokens
+        # Add batch dimension
+        print("activations device", activations.device)
+        activations = activations.unsqueeze(0)
+        # Initialize the sparse autoencoder model for the final layer.
+        sparse_acts = self.sae.encode(activations)
+        return sparse_acts
