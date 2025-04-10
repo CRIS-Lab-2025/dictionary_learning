e5cf4e91227822820e38b40aaf757d9c12677c53
Collin Szczepanski <cszcz00@gmail.com>
2025-04-09 22:44:51 -0400

    updated wrappers

--- wrappers.py
+++ wrappers.py
@@ -5,7 +5,64 @@ from dictionary import AutoEncoder
 import os
 from pathlib import Path
 
-class GPTneoX_LayerWrapper():
+class GPTneoX_DenseWrapper():
+
+    def __init__(self, mod, layer_info) -> None:
+        
+        self.model, self.tokenizer = mod
+        self.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+        self.model.resize_token_embeddings(len(self.tokenizer))
+        layer_ind, typ = layer_info
+        if typ == 'attn':  
+            layer = self.model.base_model.layers[layer_ind].attention
+        elif typ == 'mlp':
+            layer = self.model.base_model.layers[layer_ind].mlp
+        else:
+            raise NotImplementedError()
+        
+        self.layer = layer
+        self.act = []
+        def hook_fn(module, input, output):
+            self.act.append(output)
+        self.handle = self.layer.register_forward_hook(hook_fn)
+
+    def batch_activations(self, sentences, tokens='all'):
+        
+        self.act.clear() 
+        inputs = self.tokenizer(sentences, return_tensors="pt", padding=True, truncation=False)
+        toks = inputs["attention_mask"].sum(dim=1)
+        inputs = {key: val.to(self.model.device) for key, val in inputs.items()}
+
+        with torch.no_grad():
+            self.model(**inputs)
+
+        acts = torch.stack(self.act).squeeze(0)
+
+        if tokens == 'last':
+            return acts[torch.arange(len(sentences)), toks-1 , :], toks
+        elif tokens == 'all':
+            return acts, toks
+        else:
+            raise NotImplementedError()
+    
+class ActivationWrapper():
+
+    def __init__(self, mod) -> None:
+        self.mod = AutoModelForCausalLM.from_pretrained(mod)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
+        self.mod = self.mod.to(device)
+        tokenizer = AutoTokenizer.from_pretrained(mod)
+        self.tokenizer = tokenizer
+        self.model_name = mod.split('/')[-1]
+
+    def make_layer_wrapper(self, layer, typ):
+
+        if type(self.mod).__name__.endswith("GPTNeoXForCausalLM"):
+            return GPTneoX_DenseWrapper((self.mod, self.tokenizer), (layer, typ))
+        else:
+            raise NotImplementedError()
+
+class GPTneoX_SparseWrapper():
 
     def __init__(self, mod, layer_info, dictionary) -> None:
         
@@ -72,6 +129,6 @@ class DictionaryWrapper():
         dictionary = AutoEncoder.from_pretrained(os.path.join(dict_dir,'ae.pt'))
 
         if type(self.mod).__name__.endswith("GPTNeoXForCausalLM"):
-            return GPTneoX_LayerWrapper((self.mod, self.tokenizer), (layer, typ), dictionary)
+            return GPTneoX_SparseWrapper((self.mod, self.tokenizer), (layer, typ), dictionary)
         else:
             raise NotImplementedError()
