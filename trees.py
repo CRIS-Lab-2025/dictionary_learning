0250eeddd74df2a7fb97fe38dc1fcee65f91591d
Collin Szczepanski <cszcz00@gmail.com>
2025-04-09 22:37:32 -0400

    tn_to_name added

--- trees.py
+++ trees.py
@@ -6,25 +6,45 @@ class TreeMaker():
     def __init__(self, xml_file) -> None:
         tree = ET.parse(xml_file)
         root = tree.getroot()
+        names = []
         mesh_terms = {}
         for record in root.findall("DescriptorRecord"):
             ui = record.find("DescriptorUI").text
             name = record.find("DescriptorName/String").text
             tree_numbers = [tn.text for tn in record.findall("TreeNumberList/TreeNumber")]
             mesh_terms[name] = {"UI": ui, "TreeNumbers": tree_numbers}
-
+            names.append(name)
+        
+        self.names = names
         self.mesh_dict = mesh_terms
+    
+    def tn_to_name(self, tree_number):
+        
+        name = ''
+
+        for n, temp in self.mesh_dict.items():
+            tn = temp['TreeNumbers']
+            if tree_number in tn:
+
+                name = n
+                break
+
+        return namex
+
 
     def tree_from_key(self, keyword):
         codes = []
-        codes_dict = {}
+        code_names = []
         for term, val in self.mesh_dict.items():
             tn_list = val['TreeNumbers']
             for num in tn_list:
                 if keyword in num:
                     codes.append(num)
-                    codes_dict[num] = term
-        codes.sort(key=lambda x: x.count('.'))
+                    code_names.append(term)
+
+        paired = list(zip(codes, code_names))
+        paired.sort(key=lambda x: x[0].count('.'))
+        codes, code_names = zip(*paired)
         code_set = set(codes)
         G = nx.DiGraph()
         for code in codes:
@@ -34,5 +54,7 @@ class TreeMaker():
                 if parent in code_set:
                     G.add_edge(parent, code)
 
-        return G, codes_dict
+        return G, code_names
+    
+
         
