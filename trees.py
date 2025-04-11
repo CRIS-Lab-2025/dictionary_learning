db4a505a8e163408ebcece18cd19c41198de2f11
Ryan Sherby <ryansherby@ryans-mbp-4.lan>
2025-04-11 12:22:15 -0400

    trees

--- trees.py
+++ trees.py
@@ -1,22 +1,160 @@
+from typing import Dict, List
 import xml.etree.ElementTree as ET
 import networkx as nx
 
-class TreeMaker():
+
+class Record():
+    def __init__(self, name, ui, tree_numbers, note, tree):
+        self.name:str = name
+        self.ui:str = ui
+        self.tree_numbers:List[str] = tree_numbers
+        self.note:str = note
+        
+        self.tree:nx.DiGraph = tree # Tree object
+        
+    def get_tree_numbers(self):
+        return self.tree_numbers
+    
+    def add_tree_number(self, tree_number):
+        if tree_number not in self.tree_numbers:
+            self.tree_numbers.append(tree_number)
+            
+    def get_id(self):
+        return self.ui
+    
+    def get_name(self):
+        return self.name
+    
+    def get_note(self):
+        return self.note
+    
+    def get_ancestors(self,group_by_depth=False):
+        
+
+        anc = nx.ancestors(self.tree, self.ui)
+        
+        if group_by_depth:
+            
+            groups = []
+            
+            subtree = self.tree.subgraph(anc+self)
+            
+            for d in anc:
+                depth = nx.shortest_path_length(subtree, source=self, target=d)
+                
+                if len(groups) < depth:
+                    for _ in range(depth - len(groups)):
+                        groups.append([])
+                
+                groups[depth-1].append(d)
+        
+        return anc
+    
+    def get_parents(self):
+        pred = self.tree.predecessors(self)
+        
+        return list(pred)
+    
+    def get_children(self):
+        succ = self.tree.successors(self)
+        
+        return list(succ)
+    
+    def get_descendants(self,group_by_depth=False):
+        
+
+        desc = nx.descendants(self.tree, self.ui)
+        
+        if group_by_depth:
+            
+            groups = []
+            
+            subtree = self.tree.subgraph(desc+self)
+            
+            for d in desc:
+                depth = nx.shortest_path_length(subtree, source=self, target=d)
+                
+                if len(groups) < depth:
+                    for _ in range(depth - len(groups)):
+                        groups.append([])
+                
+                groups[depth-1].append(d)
+        
+        return desc
+    
+    
+    def __str__(self):
+        return f"Record(name={self.name}, ui={self.ui})"
+
+    def __repr__(self):
+        return f"Record(name={self.name}, ui={self.ui}, tree_numbers={self.tree_numbers}, note={self.note} tree={self.tree})"
+
+class Tree():
+
+
 
     def __init__(self, xml_file) -> None:
+        self.mesh_dict:Dict[Record] = {}
+        self.graph = nx.DiGraph()
+        self.name_to_id = {}
+        self.tn_to_id = {}
+        
+        
         tree = ET.parse(xml_file)
         root = tree.getroot()
-        names = []
-        mesh_terms = {}
+
         for record in root.findall("DescriptorRecord"):
+            
+            
             ui = record.find("DescriptorUI").text
             name = record.find("DescriptorName/String").text
+            name_lower = name.lower()
             tree_numbers = [tn.text for tn in record.findall("TreeNumberList/TreeNumber")]
-            mesh_terms[name] = {"UI": ui, "TreeNumbers": tree_numbers}
-            names.append(name)
+            note = record.find("ScopeNote").text if record.find("ScopeNote") is not None else None
+            
+            record = Record(name, ui, tree_numbers, note, self.graph)
+            
+            self.mesh_dict[ui] = record
+            self.name_to_id[name_lower] = ui
+            self.tn_to_id |= {tn: ui for tn in tree_numbers}
+            self.graph.add_node(record)
+            
+        for record in self.mesh_dict.values():
+            for tn in record.get_tree_numbers():
+                parent = tn.rsplit('.', 1)[0]
+                
+                parent_ui = self.tn_to_id.get(parent, None)
+                
+                if parent_ui and parent_ui != record.get_id():
+                    parent_record = self.mesh_dict[parent_ui]
+                    self.graph.add_edge(parent_record, record)
+            
+            
+            
+
+
+    
+    def get_node(self, identifier:str):
+        identifier = identifier.lower()
+
+        by_name = self.name_to_id.get(identifier, None)
+        if by_name:
+            record = self.mesh_dict.get(by_name)
+            return record
         
-        self.names = names
-        self.mesh_dict = mesh_terms
+        by_tn = self.tn_to_id.get(identifier, None)
+        if by_tn:
+            record = self.mesh_dict.get(by_tn)
+            return record
+        
+        record = self.mesh_dict.get(identifier, None)
+        if record:
+            return record
+        
+        
+        return None
+    
+        
     
     def tn_to_name(self, tree_number):
         
